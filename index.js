14fae55e428359a6f6daaf01d1fec6c6d1a34992
derud <122689893+derud@users.noreply.github.com>
2023-02-03 16:34:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+
+//prompt("Qual o seu nome?")
+//prompt("Qual o nome da pessoa?")
+//var CalculadoraDoAmor = Math.random() *100;
+//CalculadoraDoAmor = Math.floor(CalculadoraDoAmor)+1;
+//console.log(CalculadoraDoAmor);
+//alert("A probabilidade de amor é "+ CalculadoraDoAmor + "%")
+
+
+
+prompt("Qual o seu nome?")
+prompt("Qual o nome da pessoa?")
+var CalculadoraDoAmor = Math.random() * 100;
+CalculadoraDoAmor = Math.floor(CalculadoraDoAmor) + 1;
+
+if (CalculadoraDoAmor > 70) {
+    alert("A probabilidade de amor é " + CalculadoraDoAmor + "%" + " Esse vc consegue namorar!");
+}
+
+if (CalculadoraDoAmor > 30 && CalculadoraDoAmor <= 70) {
+    alert("Seu nivel de amor é " + CalculadoraDoAmor + "%");
+}
+
+if (CalculadoraDoAmor <= 30) {
+    alert("Seu nível de amor é " + CalculadoraDoAmor + " % " + "vocês dois se dão bem como água e óleo!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
